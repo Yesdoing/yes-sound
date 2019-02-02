6a5359f13621377bceb68a3e11786cbef8522a22
Jane Lee <Yesdoing13@gmail.com>
2019-02-02 14:06:37 +0900

    config redux settings

---
+++ src/store/modules/index.js
@@ -0,0 +1,6 @@
+export { default as player } from './player';
+export { default as playerList } from './playerList';
+export { default as post } from './post';
+export { default as list } from './list';
+export { default as base } from './base';
+export { penderReducer as pender } from 'redux-pender';
