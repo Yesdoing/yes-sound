83011bf4752259e250161b47f35ba2a3e810e09c
Jane Lee <Yesdoing13@gmail.com>
2019-02-02 14:44:36 +0900

    config page route

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom';
+import { ListPage, PostPage, NotFoundPage } from 'pages';
+ 
 const App = () => {
     return (
         <div>
-            yes-sound App!!
+            <Switch>
+                <Route exact path="/" component={ListPage} />
+                <Route path="/page/:tag/:page?" component={ListPage} />
+                <Route path="/search/:keyword/:page?" component={ListPage} />
+                <Route path="/songs/:id" component={PostPage} />
+                <Route path="/users/:id" component={PostPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
         </div>
     );
 };
